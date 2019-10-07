2b7d9221b581c1f2dcdfcfcafa59f81cd6604269
spinon187 <spinon187@gmail.com>
2019-10-07 12:17:11 -0500

    cleaning out leftover crap

--- back/spectre-r/app/models/message.rb
+++
@@ -1,8 +0,0 @@
-class Message
-  include Mongoid::Document
-  include Mongoid::Timestamps
-  field :from, type: String
-  field :to, type: String
-  field :msg, type: String
-  field :when, type: Timestamp
-end
